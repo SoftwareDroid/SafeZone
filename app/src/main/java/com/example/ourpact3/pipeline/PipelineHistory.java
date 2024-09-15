7cbc4acd924f298ea305754793ce3a70dd0daef7
Patrick <patrickmis@web.de>
2024-09-15 18:58:41 +0200

    more to histroy object

--- app/src/main/java/com/example/ourpact3/pipeline/PipelineHistory.java
+++ app/src/main/java/com/example/ourpact3/pipeline/PipelineHistory.java
@@ -4,6 +4,9 @@ import com.example.ourpact3.smart_filter.SpecialSmartFilterBase;
 import com.example.ourpact3.smart_filter.WordProcessorSmartFilterBase;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class PipelineHistory
 {
@@ -14,22 +17,35 @@ public class PipelineHistory
         String explainationShort = "";
         String resultIconChar = ""; //e.g Skull for killing
     }
-    
+
     private ArrayList<SpecialSmartFilterBase> runSmartFilters1;
     private ArrayList<WordProcessorSmartFilterBase> runSmartFilters2;
     public int number = 0;
+
+    public List<WordProcessorSmartFilterBase> getSmartFilters2()
+    {
+        return Collections.unmodifiableList(runSmartFilters2);
+    }
+
+    public List<SpecialSmartFilterBase> getSpecialFilters()
+    {
+        return Collections.unmodifiableList(runSmartFilters1);
+    }
+
     PipelineHistory()
     {
-        
+
     }
+
     /**
      * @param filter
      */
-    public void addSpecialFilter(SpecialSmartFilterBase filter,ResultWithExplaination resultWithExplaination)
+    public void addSpecialFilter(SpecialSmartFilterBase filter, ResultWithExplaination resultWithExplaination)
     {
         number += 1;
     }
-    public void addWorProcessorFilter(WordProcessorSmartFilterBase filter,ResultWithExplaination resultWithExplaination)
+
+    public void addWorProcessorFilter(WordProcessorSmartFilterBase filter, ResultWithExplaination resultWithExplaination)
     {
         runSmartFilters2.add(filter);
     }
