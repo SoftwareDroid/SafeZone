d27062da8f3d1aa6ba322dfcea00df8a86440101
Patrick <patrickmis@web.de>
2024-12-21 09:49:52 +0100

    update terms

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.19.2"
+        versionName = "1.19.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
