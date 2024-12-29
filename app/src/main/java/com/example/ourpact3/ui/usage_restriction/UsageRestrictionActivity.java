b8fdfbef5d71f4f31c84d3ef9457a5cbaf152308
Patrick <patrickmis@web.de>
2024-12-29 08:20:18 +0100

    add weekday selector

--- app/src/main/java/com/example/ourpact3/ui/usage_restriction/UsageRestrictionActivity.java
+++ app/src/main/java/com/example/ourpact3/ui/usage_restriction/UsageRestrictionActivity.java
@@ -1,6 +1,8 @@
 package com.example.ourpact3.ui.usage_restriction;
 
+import java.time.DayOfWeek;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import android.app.TimePickerDialog;
@@ -14,11 +16,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.ourpact3.R;
+import com.example.ourpact3.model.PipelineButtonAction;
+import com.example.ourpact3.ui.settings.ReusableSettingsCheckboxView;
+import com.example.ourpact3.ui.settings.ReusableSettingsComboboxViev;
 import com.example.ourpact3.ui.settings.ReusableSettingsDurationInputView;
 import com.example.ourpact3.ui.settings.ReusableSettingsItemView;
 import com.example.ourpact3.ui.settings.ReusableSettingsNumberInputView;
 import com.example.ourpact3.ui.settings.ReusableSettingsTimePickerInputView;
 
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class UsageRestrictionActivity extends AppCompatActivity
@@ -29,7 +36,7 @@ public class UsageRestrictionActivity extends AppCompatActivity
     private ReusableSettingsDurationInputView resetPeriodInput;
     private ReusableSettingsTimePickerInputView selectedStartInput;
     private ReusableSettingsTimePickerInputView selectedEndInput;
-
+    private ReusableSettingsCheckboxView<DayOfWeek> weekdaySelector;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -62,6 +69,23 @@ public class UsageRestrictionActivity extends AppCompatActivity
         selectedStartInput.setParameters(this.getString(R.string.pick_start_time), "%02d:%02d", LocalTime.now());
         selectedEndInput = new ReusableSettingsTimePickerInputView(this, findViewById(R.id.setting_input_end_time));
         selectedEndInput.setParameters(this.getString(R.string.pick_end_time), "%02d:%02d", LocalTime.now());
+        // weekday picker
+        weekdaySelector = new ReusableSettingsCheckboxView<DayOfWeek>(this, findViewById(R.id.setting_input_weekdays));
+        LinkedHashMap<DayOfWeek, String> values = new LinkedHashMap<DayOfWeek, String>();
+        values.put(DayOfWeek.MONDAY, this.getString(R.string.monday));
+        values.put(DayOfWeek.TUESDAY, this.getString(R.string.tuesday));
+        values.put(DayOfWeek.WEDNESDAY, this.getString(R.string.wednesday));
+        values.put(DayOfWeek.THURSDAY, this.getString(R.string.thursday));
+        values.put(DayOfWeek.FRIDAY, this.getString(R.string.friday));
+        values.put(DayOfWeek.SATURDAY, this.getString(R.string.saturday));
+        values.put(DayOfWeek.SUNDAY, this.getString(R.string.sunday));
+        List<DayOfWeek> initialSelection = new ArrayList<DayOfWeek>();
+        initialSelection.add(DayOfWeek.MONDAY);
+        initialSelection.add(DayOfWeek.TUESDAY);
+        initialSelection.add(DayOfWeek.WEDNESDAY);
+        initialSelection.add(DayOfWeek.THURSDAY);
+        initialSelection.add(DayOfWeek.FRIDAY);
+        weekdaySelector.setParameters(this.getString(R.string.choose_a_option), "Applied on: %s", values, initialSelection);
 
     }
 
