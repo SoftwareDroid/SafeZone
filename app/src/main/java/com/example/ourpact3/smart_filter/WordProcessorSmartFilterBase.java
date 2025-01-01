23e4bf75c7d9341a169fe5b7f7bf52e9a936f81d
Patrick <patrickmis@web.de>
2025-01-01 12:18:17 +0100

    started content filter adapter and layout

--- app/src/main/java/com/example/ourpact3/smart_filter/WordProcessorSmartFilterBase.java
+++ app/src/main/java/com/example/ourpact3/smart_filter/WordProcessorSmartFilterBase.java
@@ -6,54 +6,103 @@ import com.example.ourpact3.pipeline.PipelineResultBase;
 import com.example.ourpact3.pipeline.PipelineResultKeywordFilter;
 import com.example.ourpact3.service.ScreenInfoExtractor;
 
-public abstract class WordProcessorSmartFilterBase implements Cloneable {
+/**
+ * Can be of different types const lists or reusable
+ */
+public abstract class WordProcessorSmartFilterBase implements Cloneable
+{
+    private String shortDescription;
+    private boolean enabled;
     private PipelineResultKeywordFilter constResult; // Made private
     private boolean checkOnlyVisibleNodes = true; // Made private
     public final WordSmartFilterIdentifier identifier;
-    WordProcessorSmartFilterBase(PipelineResultKeywordFilter constResult, WordSmartFilterIdentifier identifier) throws CloneNotSupportedException {
+
+    WordProcessorSmartFilterBase(PipelineResultKeywordFilter constResult, WordSmartFilterIdentifier identifier) throws CloneNotSupportedException
+    {
         this.constResult = (PipelineResultKeywordFilter) constResult.clone();
         this.identifier = identifier;
+        this.enabled = true;
+        this.shortDescription = "";
     }
 
-    public void setName(String name )
+    public void setEnabled(boolean value)
+    {
+        this.enabled = value;
+    }
+
+    public boolean isEnabled()
+    {
+        return this.enabled;
+    }
+
+    /**
+     * Returns the used defined name of the filter
+     *
+     * @return
+     */
+    public String getName()
+    {
+        return constResult.getTriggerFilter();
+    }
+
+    public void setFilterShortDescription(String text)
+    {
+        shortDescription = text;
+    }
+
+    public String getFilterShortDescription()
+    {
+        return shortDescription;
+    }
+
+    public void setName(String name)
     {
         constResult.setTriggerFilter(name);
     }
+
     @NonNull
     @Override
-    public WordProcessorSmartFilterBase clone() {
-        try {
+    public WordProcessorSmartFilterBase clone()
+    {
+        try
+        {
             WordProcessorSmartFilterBase clone = (WordProcessorSmartFilterBase) super.clone();
             // Deep copy the constResult
             clone.constResult = (PipelineResultKeywordFilter) this.constResult.clone();
             // Note: name is final and immutable, so no need to clone it
             clone.checkOnlyVisibleNodes = this.checkOnlyVisibleNodes; // Copy the boolean field
             return clone;
-        } catch (CloneNotSupportedException e) {
+        } catch (CloneNotSupportedException e)
+        {
             throw new AssertionError(); // Can't happen since we are Cloneable
         }
     }
 
     // Getter for constResult
-    public PipelineResultKeywordFilter getConstResult() {
+    public PipelineResultKeywordFilter getConstResult()
+    {
         return constResult;
     }
 
     // Setter for constResult
-    public void setConstResult(PipelineResultKeywordFilter constResult) throws CloneNotSupportedException {
+    public void setConstResult(PipelineResultKeywordFilter constResult) throws CloneNotSupportedException
+    {
         this.constResult = (PipelineResultKeywordFilter) constResult.clone();
     }
 
     // Getter for checkOnlyVisibleNodes
-    public boolean isCheckOnlyVisibleNodes() {
+    public boolean isCheckOnlyVisibleNodes()
+    {
         return checkOnlyVisibleNodes;
     }
 
     // Setter for checkOnlyVisibleNodes
-    public void setCheckOnlyVisibleNodes(boolean checkOnlyVisibleNodes) {
+    public void setCheckOnlyVisibleNodes(boolean checkOnlyVisibleNodes)
+    {
         this.checkOnlyVisibleNodes = checkOnlyVisibleNodes;
     }
 
     public abstract PipelineResultBase feedWord(ScreenInfoExtractor.Screen.TextNode textNode);
+
     public abstract void reset();
 }
---
+++ app/src/main/java/com/example/ourpact3/ui/content_restriction_settings_for_app/ContentFilterRuleAdapter.java
@@ -0,0 +1,203 @@
+package com.example.ourpact3.ui.content_restriction_settings_for_app;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ActionMode;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ourpact3.R;
+import com.example.ourpact3.smart_filter.WordProcessorSmartFilterBase;
+import androidx.appcompat.view.ActionMode.Callback;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContentFilterRuleAdapter extends RecyclerView.Adapter<ContentFilterRuleAdapter.ViewHolder>
+{
+    private List<WordProcessorSmartFilterBase> items;
+    private Context context;
+    private int selectedItemPosition = RecyclerView.NO_POSITION; // No selection initially
+
+    public ContentFilterRuleAdapter(Context context)
+    {
+        this.items = new ArrayList<>();
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        View view = LayoutInflater.from(context).inflate(R.layout.list_entry_time_rule, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        viewHolder.items = items;   // give the view access
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, @SuppressLint("RecyclerView") int position)
+    {
+        WordProcessorSmartFilterBase rule = items.get(position);
+        holder.isEnabledSwitch.setChecked(rule.isEnabled());
+        holder.title.setText(rule.getName());
+        holder.shortDescription.setText(rule.getFilterShortDescription());
+        // Highlight selected item
+        holder.itemView.setBackgroundColor(position == selectedItemPosition ? context.getColor(R.color.white)  : context.getColor(R.color.purple_200));
+        // Set the OnCheckedChangeListener for the Switch
+        holder.isEnabledSwitch.setOnCheckedChangeListener(null); // Clear previous listener to avoid unwanted triggers
+        holder.isEnabledSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            // Update the isEnabled attribute of the rule
+            rule.setEnabled(isChecked);
+        });
+        ActionModeCallback myActionMode = new ActionModeCallback();
+        // Set the OnLongClickListener to start Contextual Action Mode
+        holder.itemView.setOnLongClickListener(v -> {
+            selectedItemPosition = position; // Select the item
+            ((AppCompatActivity) context).startSupportActionMode(myActionMode);
+            notifyDataSetChanged();
+            return true;
+        });
+
+    }
+
+    public List<WordProcessorSmartFilterBase> getAllItems()
+    {
+        return items;
+    }
+
+    public void addEntry(WordProcessorSmartFilterBase entry)
+    {
+        items.add(entry);
+        notifyItemInserted(items.size() - 1);
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return items.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView title;
+        TextView shortDescription;
+        @SuppressLint("UseSwitchCompatOrMaterialCode")
+        Switch isEnabledSwitch;
+        List<WordProcessorSmartFilterBase> items;
+
+        public ViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+            title = itemView.findViewById(R.id.item_main_text);
+            shortDescription = itemView.findViewById(R.id.content_filter_short_description);
+            isEnabledSwitch = itemView.findViewById(R.id.is_enabled);
+        }
+    }
+
+
+    private class ActionModeCallback implements ActionMode.Callback
+    {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu)
+        {
+            mode.getMenuInflater().inflate(R.menu.context_menu_context_filter, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu)
+        {
+            mode.setTitle("1 item selected");
+            return false;
+        }
+
+        private void moveSelectedItemUp()
+        {
+            if (selectedItemPosition > 0)
+            {
+                // Get the item to move
+                WordProcessorSmartFilterBase item = items.get(selectedItemPosition);
+
+                // Remove the item from its current position
+                items.remove(selectedItemPosition);
+
+                // Insert it one position up
+                items.add(selectedItemPosition - 1, item);
+                notifyItemMoved(selectedItemPosition, selectedItemPosition - 1);
+                // Update the selected position
+                selectedItemPosition--;
+
+            }
+        }
+
+        private void moveSelectedItemDown()
+        {
+            if (selectedItemPosition < items.size() - 1)
+            {
+                // Get the item to move
+                WordProcessorSmartFilterBase item = items.get(selectedItemPosition);
+
+                // Remove the item from its current position
+                items.remove(selectedItemPosition);
+
+                // Insert it one position down
+                items.add(selectedItemPosition + 1, item);
+
+                // Update the selected position
+                notifyItemMoved(selectedItemPosition, selectedItemPosition + 1);
+
+                selectedItemPosition++;
+            }
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item)
+        {
+            if (item.getItemId() == R.id.action_delete)
+            {
+//                deleteSelectedItem();
+                mode.finish(); // Close the action mode
+                return true;
+            } else if (item.getItemId() == R.id.action_open)
+            {
+//                openSelectedItem();
+                mode.finish(); // Close the action mode
+                return true;
+            } else if (item.getItemId() == R.id.action_move_up)
+            {
+                moveSelectedItemUp();
+                mode.finish(); // Close the action mode
+                return true;
+            } else if (item.getItemId() == R.id.action_move_down)
+            {
+                moveSelectedItemDown();
+                mode.finish(); // Close the action mode
+                return true;
+            } else if (item.getItemId() == R.id.action_words)
+            {
+//                showWordsForSelectedItem();
+                mode.finish(); // Close the action mode
+                return true;
+            }
+            return false; // Return false if no action was handled
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode)
+        {
+            selectedItemPosition = RecyclerView.NO_POSITION;
+            notifyDataSetChanged();
+        }
+    }
+
+
+}
